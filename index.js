58ebba831b82681a677b05c46a6ccfc16fa8b314
Utsav Kumar Singh <127019911+uts8434@users.noreply.github.com>
2024-08-25 16:31:42 +0530

    Resolved  vercer

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
+import express from 'express';
+import nodemailer from 'nodemailer';
+import bodyParser from 'body-parser';
+
+// Create Express app
 const app = express();
 const port = 8080;
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
 
 // Serve static files from 'public' directory
 app.use(express.static('public'));
@@ -21,18 +23,13 @@ const transporter = nodemailer.createTransport({
 });
 
 app.post('/send-email', (req, res) => {
-    console.log('Request Body:', req.body); // Log the incoming body
-    console.log('Contact Name:', req.body.contactName);
-    console.log('Contact Email:', req.body.contactEmail);
-    console.log('Contact Subject:', req.body.contactSubject);
-    console.log('Contact Message:', req.body.contactMessage);
-
     const { contactName, contactEmail, contactSubject, contactMessage } = req.body;
 
     // Validate input
     if (!contactName || !contactEmail || !contactMessage) {
         return res.status(400).json({ success: false, message: 'Invalid input' });
     }
+
     const mailOptions = {
         from: contactEmail,
         to: 'utsav123k@gmail.com',
@@ -45,12 +42,10 @@ app.post('/send-email', (req, res) => {
             console.error('Error sending email:', error);
             return res.status(500).json({ success: false, message: 'Error sending email' });
         }
-        console.log('Email sent:', info.response);
         res.json({ success: true, message: 'Email sent' });
     });
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
